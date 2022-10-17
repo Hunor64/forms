944907fb590a0ceef9edecea79d8a97e9321e8b5
Hunor József Nagy <Nhunor06@gmail.com>
2022-10-17 12:37:05 +0200

    1. Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function kiemel() {
+    var beviteliMezo = document.getElementById("nev");
+    beviteliMezo.style.fontSize="2em";
+    beviteliMezo.style.backgroundColor="Yellow";   
+    beviteliMezo.style.border="5px solid red";
+    beviteliMezo.style.borderStyle="round";
+}
+function alaphelyzet() {
+    var beviteliMezo = document.getElementById("nev");
+    beviteliMezo.style.fontSize="1em";
+    beviteliMezo.style.backgroundColor="White";
+    beviteliMezo.style.border="1px solid black";
+    beviteliMezo.style.borderStyle="round";
+}
